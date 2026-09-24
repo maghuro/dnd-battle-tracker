17598931a3e16a982eceac392f22f1e553951395
Maghuro <1546139+maghuro@users.noreply.github.com>
2026-09-24 11:44:31 +0100

    Enable Web Crypto in browser tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,17 @@
 import 'cross-fetch/polyfill';
+import { webcrypto } from 'crypto';
+import { TextDecoder, TextEncoder } from 'util';
 import server from './test-integration/mocks/server';
+
+if (!window.crypto?.subtle) {
+  Object.defineProperty(window, 'crypto', {
+    configurable: true,
+    value: webcrypto,
+  });
+}
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 window.HTMLElement.prototype.scrollIntoView = () => {};
 window.scrollTo = () => {};
